778a0588021552e8c52d077289f38b4d3389dc96
Kennedy Owusu <kennediowusu@gmail.com>
2023-07-20 18:31:40 +0000

    add project endpoint

---
+++ lib/service/endpoint.dart
@@ -0,0 +1,8 @@
+const String bASEURL = "http://0.0.0.0:8000/api/";
+const String loginURL = "${bASEURL}login";
+const String registerURL = "${bASEURL}register";
+const String logoutURL = "${bASEURL}logout";
+const String userURL = "${bASEURL}user";
+const String productURL = "${bASEURL}products";
+const String categoryURL = "${bASEURL}categories";
+const String cartURL = "${bASEURL}cart";
