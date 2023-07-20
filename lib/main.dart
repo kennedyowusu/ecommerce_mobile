52dd1126f4cbc55cd3261118271042791f07a804
Kennedy Owusu <kennediowusu@gmail.com>
2023-07-20 18:35:10 +0000

    check token existence in redirect based on that

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,34 @@
 import 'package:ecommerce_ui/views/authentication/login.dart';
+import 'package:ecommerce_ui/views/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(
-    const ProviderScope(
+    ProviderScope(
       child: Ecommerce(),
     ),
   );
 }
 
 class Ecommerce extends ConsumerWidget {
-  const Ecommerce({super.key});
+  Ecommerce({super.key});
+
+  final String title = 'E-commerce';
+  final GetStorage storage = GetStorage();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'E-commerce',
+      title: title,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginView(),
+      home: storage.hasData('token') ? const HomeView() : LoginView(),
     );
   }
 }
