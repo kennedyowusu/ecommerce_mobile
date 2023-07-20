b51b6a0cfdfff7ca4c1a7cf64fd6a5c5a1977e2b
Kennedy Owusu <kennediowusu@gmail.com>
2023-07-20 22:06:22 +0000

    create category model

---
+++ lib/models/category_model.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+CategoryModel categoryModelFromJson(String str) =>
+    CategoryModel.fromJson(json.decode(str));
+
+String categoryModelToJson(CategoryModel data) => json.encode(data.toJson());
+
+class CategoryModel {
+  List<Category> data;
+
+  CategoryModel({
+    required this.data,
+  });
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) => CategoryModel(
+        data:
+            List<Category>.from(json["data"].map((x) => Category.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+      };
+}
+
+class Category {
+  int id;
+  String name;
+
+  Category({
+    required this.id,
+    required this.name,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+      };
+}
