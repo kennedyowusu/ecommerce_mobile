863119df162f6699c1f37b880a3f284afe9b63c9
Kennedy Owusu <kennediowusu@gmail.com>
2023-07-21 20:31:07 +0000

    retrieve and display cart items

--- lib/views/cart.dart
+++ lib/views/cart.dart
@@ -1,4 +1,7 @@
 import 'package:ecommerce_ui/controllers/cart_items_controller.dart';
+import 'package:ecommerce_ui/models/cart_model.dart';
+import 'package:ecommerce_ui/models/products_model.dart';
+import 'package:ecommerce_ui/providers/providers.dart';
 import 'package:ecommerce_ui/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,159 +14,183 @@ class ProductCartView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final cartItem = ref.watch(itemBagProvider);
+    // final cartItem = ref.watch(itemBagProvider);
+    final AsyncValue<CartResponseModel> cartItem =
+        ref.watch(cartNotifierProvider);
+
+    final AsyncValue<ProductResponseModel> productController =
+        ref.watch(productNotifierProvider);
+
+    debugPrint('Items in cart: $cartItem');
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size(double.infinity, 60.0),
-        child: CustomAppBar(cartItem: cartItem, title: 'Cart'),
+        child: CustomAppBar(
+          cartItem: [],
+          title: 'Cart',
+        ),
       ),
-      body: Column(children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            color: kLightBackground,
-            padding: const EdgeInsets.all(20),
-            child: ListView.builder(
-              itemCount: cartItem.length,
-              itemBuilder: (context, index) => Card(
-                child: Container(
-                  color: Colors.white,
-                  width: double.infinity,
-                  child: Row(children: [
-                    Expanded(
-                      flex: 1,
-                      child: Image.asset(cartItem[index].image),
+      body: SafeArea(
+        child: Column(children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              color: kLightBackground,
+              child: ListView.builder(
+                itemCount: cartItem.when(
+                  data: (data) => data.data.length,
+                  error: (error, stackTrace) => 0,
+                  loading: () => 0,
+                ),
+                itemBuilder: (context, index) => Card(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    Expanded(
-                        flex: 3,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                cartItem[index].name,
-                                style: AppTheme.kCardTitle,
-                              ),
-                              const Gap(6),
-                              Text(
-                                cartItem[index].description,
-                                style: AppTheme.kBodyText,
-                              ),
-                              const Gap(4),
-                              Text(
-                                '\$${cartItem[index].price}',
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              )
-                            ],
-                          ),
-                        ))
-                  ]),
+                    width: double.infinity,
+                    child: ListTile(
+                      // leading: Image.network(
+                      //   productController.when(
+                      //     data: (data) => data.data[index].image,
+                      //     error: (error, stackTrace) => '',
+                      //     loading: () => '',
+                      //   ),
+                      // ),
+
+                      leading: Icon(Icons.image),
+                      title: Text(
+                        productController.when(
+                          data: (data) => data.data[index].name,
+                          error: (error, stackTrace) => '',
+                          loading: () => '',
+                        ),
+                        style: AppTheme.kCardTitle,
+                      ),
+                      // subtitle: Text(
+                      //   productController.when(
+                      //     data: (data) => data.data[index].description,
+                      //     error: (error, stackTrace) => '',
+                      //     loading: () => '',
+                      //   ),
+                      //   style: AppTheme.kBodyText,
+                      // ),
+                      trailing: Text(
+                        '\$${productController.when(
+                          data: (data) => data.data[index].price,
+                          error: (error, stackTrace) => '',
+                          loading: () => '',
+                        )}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-            flex: 1,
-            child: Container(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Have a coupon code? enter here'),
-                  const Gap(12),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    height: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        width: 1,
-                        color: kPrimaryColor,
+          Expanded(
+              flex: 1,
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Have a coupon code? enter here'),
+                    const Gap(12),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          width: 1,
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'FDS2023',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            children: const [
+                              Text(
+                                'Available',
+                                style: TextStyle(
+                                    color: kPrimaryColor,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Gap(5),
+                              Icon(Icons.check_circle)
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                    child: Row(
+                    const Gap(12),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'FDS2023',
+                      children: const [
+                        Text(
+                          'Delivery Fee:',
                           style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
+                              fontSize: 18, fontWeight: FontWeight.w400),
                         ),
-                        Row(
-                          children: const [
-                            Text(
-                              'Available',
-                              style: TextStyle(
-                                  color: kPrimaryColor,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Gap(5),
-                            Icon(Icons.check_circle)
-                          ],
+                        Text(
+                          'Free',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w400),
                         ),
                       ],
                     ),
-                  ),
-                  const Gap(12),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        'Delivery Fee:',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w400),
-                      ),
-                      Text(
-                        'Free',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w400),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        'Discount:',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w400),
-                      ),
-                      Text(
-                        'No discount',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w400),
-                      ),
-                    ],
-                  ),
-                  const Divider(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Total:',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: kPrimaryColor),
-                      ),
-                      Text(
-                        '\$${ref.watch(priceCalcProvider)}',
-                        style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: kPrimaryColor),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ))
-      ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Discount:',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w400),
+                        ),
+                        Text(
+                          'No discount',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Total:',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: kPrimaryColor),
+                        ),
+                        Text(
+                          '\$${ref.watch(priceCalcProvider)}',
+                          style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: kPrimaryColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ))
+        ]),
+      ),
     );
   }
 }
